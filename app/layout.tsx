1bcb9c6d33db8ccecd812dfd29555ee5af056a29
hilal1709 <hilal1709@gmail.com>
2026-08-13 00:05:03 +0700

    tambah logo di tab browser

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,9 @@ const inter = Inter({ subsets: ["latin"] })
 export const metadata: Metadata = {
   title: "Desa Kedungrejo",
   description: "Website resmi Desa Kedungrejo",
+  icons: {
+    icon: "/images/logokedungrejo.jpeg",
+  },
 }
 
 export default function RootLayout({
